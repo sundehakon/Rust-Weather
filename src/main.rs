6fc2aa70a3e58276a3748e8d438d1b416da12397
sundehakon <hakon.su@gmail.com>
2024-09-19 07:53:46 +0000

    Add Rust files, begin code production

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+pub fn get_weather(city_name: String) {
+    let api_key = std::env::var("APIKEY").expect(
+        "Environment variable APIKEY not set. Please set it to your OpenWeatherMap API key.",
+    );
+
+    let url = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    )
 }
